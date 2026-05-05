db3f39a64a66c5a03c2789fcf6a138af0426eab4
Ubuntu <ubuntu@ip-172-31-35-154.ec2.internal>
2026-05-05 08:58:06 +0000

    okay

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-public class Hello {
+public class HelloWorld {
     public static void main(String[] args) {
         // This prints "Hello, World!" to the console
         System.out.println("Hello, World!");
